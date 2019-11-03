51a0dbf3d337e991541dafb7c364ee4a124b9d40
Diegore Gutierrez <dgutierrez@ia.com.mx>
2019-11-03 03:59:03 -0600

    Site 95%

--- Content/scripts/actions.js
+++ Content/scripts/actions.js
@@ -1,6 +1,11 @@
 var $wW = $(window).width(),
 		$modLink = $(".ap-mod"),
 		$mClose = $(".ap-modal-close"),
+		$infoHeight = "",
+		$infoSize = "",
+		$tl ="",
+		$ww = $(window).width(),
+		p,
 		$mCloseBtn = $('.ap-mod-close');
 
 function sliderHome() {
@@ -14,8 +19,23 @@ function sliderHome() {
 	$('.ap-slide-home-img').imagefill();
 }
 
+function imageFill() {
+	if ($('.ap-us').length) {
+		$('.ap-us-img').imagefill();
+	}
+
+	if ($('.ap-prtc-img').length) {
+		$('.ap-prtc-img').imagefill();
+	}
+	if ($('.ap-gal-img').length) {
+		$('.ap-gal-img').imagefill({runOnce:true});
+	}
+}
+
 function sliderGallery() {
-	if ($('.ap-modal-gallery-slide').length) {
+	$video = $('.ap-video-src').get(0);
+
+	if ($('#ap-gallery').length) {
 		var swiper = new Swiper('.ap-gallery-slide', {
 				loop: true,
 				pagination: {
@@ -25,6 +45,31 @@ function sliderGallery() {
 					delay: 5000,
 				}
 		});
+
+		function videoControl() {
+			$video.play();
+
+			$('.ap-video-src').bind('ended', function(){
+				$('.ap-gal-overlay').fadeOut();
+			});
+		}
+
+		$('.ap-gal-link').on('click', function(e){
+			e.preventDefault();
+			var $tar = $(this).attr('href');
+
+			$($tar).fadeIn("1", function(){
+				videoControl();
+			});
+		})
+
+		$('.ap-gal-close').on('click', function(e){
+			e.preventDefault();
+			$(this).parent().fadeOut("slow", function(){
+				$video.pause();
+			});
+		});
+
 	}
 }
 
@@ -76,6 +121,22 @@ function modalBox() {
 	}
 }
 
+function urlCntrol() {
+	var $page = jQuery.url.attr("file");
+
+	if(!$page) {
+		$page = 'index.html';
+	}
+	$('.ap-nav-cont .ap-nav-list .ap-nav-item').each(function(){
+		var	$href = this.href.substring(this.href.lastIndexOf('/') + 1);
+		if ( ($href == $page) || ($href == '') ) {
+			$(this).parent().addClass('ap-nav-list--current');
+		} else {
+			$(this).parent().removeClass('ap-nav-list--current');
+		}
+	});
+}
+
 function enter() {
 	$('.ap-intro-close').on('click', function (e) {
 		e.preventDefault();
@@ -93,7 +154,6 @@ function tabs() {
 	if ($insights.length) {
 		$listItem.on('click', function(e) {
 			e.preventDefault();
-			console.log('Holi')
 			$(this).parent().addClass("ap-tabs-item--current");
 			$(this).parent().siblings().removeClass("ap-tabs-item--current");
 			var tab = $(this).attr("href");
@@ -104,15 +164,46 @@ function tabs() {
 	}
 }
 
+
+function updateScrollBar() {
+	$(".ap-scroll").perfectScrollbar("update");
+}
+
+
+
+function infoHeight() {
+	$ww = $(window).width();
+
+	if($ww >= 760) {
+		// Si es ipad
+		$infoHeight = "30";
+		$infoSize = 'ipad';
+
+		return $infoSize
+		return $infoHeight
+	} else {
+		// Menos que ipad
+		$infoHeight = "18";
+
+		return $infoHeight
+	}
+}
+
 $(function(){
-	$('.ap-prtc-img').imagefill();
+	urlCntrol();
+	infoHeight();
+	imageFill();
 	sliderHome();
 	sliderGallery();
 	modalBox();
 	tabs();
 	enter();
-});
 
-$(document).ready(function(){
+	if ($(".ap-scroll").length) {
+		$(".ap-scroll").perfectScrollbar();
+	}
 
+	$(window).smartresize(function(){
+		infoHeight();
+	});
 });
